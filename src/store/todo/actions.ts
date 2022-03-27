bce568381dcab3ce9ac20b07ce4c877d0c99d99e
jinho1011 <gino9940@gmail.com>
2022-03-27 15:19:06 +0900

    feat: export returnType

--- src/store/todo/actions.ts
+++ src/store/todo/actions.ts
@@ -4,17 +4,23 @@ export const MODIFY = "MODIFY";
 export const DELETE = "DELETE";
 
 export const createTodo = (todo: string) => {
-  return { type: CREATE, todo };
+  return { type: CREATE, payload: { todo } } as const;
 };
 
 export const checkTodo = (id: number) => {
-  return { type: CHECK, id };
+  return { type: CHECK, payload: { id } } as const;
 };
 
 export const modifyTodo = (id: number, todo: string) => {
-  return { type: MODIFY, id, todo };
+  return { type: MODIFY, payload: { id, todo } } as const;
 };
 
 export const deleteTodo = (id: number) => {
-  return { type: DELETE, id };
+  return { type: DELETE, payload: { id } } as const;
 };
+
+export type Action =
+  | ReturnType<typeof createTodo>
+  | ReturnType<typeof checkTodo>
+  | ReturnType<typeof modifyTodo>
+  | ReturnType<typeof deleteTodo>;
