47dcb34bfc85e495f6ba4f1cd391c5a7e1181c2e
jinho1011 <gino9940@gmail.com>
2022-03-27 15:28:47 +0900

    feat: implement todo reducer

--- src/store/todo/reducer.ts
+++ src/store/todo/reducer.ts
@@ -1,7 +1,73 @@
-const initialState = {};
+import { CREATE, CHECK, MODIFY, DELETE, Action } from "./actions";
 
-const Reducer = (state = initialState, action: any) => {
+interface Todo {
+  id: number;
+  todo: string;
+  check: boolean;
+}
+
+interface State {
+  data: Todo[];
+}
+
+const initialState: State = {
+  data: [],
+};
+
+const Reducer = (state = initialState, action: Action) => {
   switch (action.type) {
+    case CREATE: {
+      const newTodo: Todo = {
+        id: Date.now(),
+        todo: action.payload.todo,
+        check: false,
+      };
+
+      return {
+        ...state,
+        data: [...state.data, newTodo],
+      };
+    }
+    case CHECK: {
+      const checkedTodos = state.data.map((todo) => {
+        if (todo.id === action.payload.id)
+          return {
+            ...todo,
+            check: !todo.check,
+          };
+        else return todo;
+      });
+
+      return {
+        ...state,
+        data: checkedTodos,
+      };
+    }
+    case MODIFY: {
+      const modifiedTodos = state.data.map((todo) => {
+        if (todo.id === action.payload.id)
+          return {
+            ...todo,
+            todo: action.payload.todo,
+          };
+        else return todo;
+      });
+
+      return {
+        ...state,
+        data: modifiedTodos,
+      };
+    }
+    case DELETE: {
+      const deletedTodos = state.data.filter(
+        (todo) => todo.id !== action.payload.id
+      );
+
+      return {
+        ...state,
+        data: deletedTodos,
+      };
+    }
     default:
       return state;
   }
