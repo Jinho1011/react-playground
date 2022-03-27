488e8aad17933133cb43399d1bed7107ca7e541a
jinho1011 <gino9940@gmail.com>
2022-03-27 15:11:24 +0900

    feat: add todo actions

--- src/store/todo/actions.ts
+++ src/store/todo/actions.ts
@@ -1,5 +1,20 @@
-export const ACTION = "ACTION";
+export const CREATE = "CREATE";
+export const CHECK = "CHECK";
+export const MODIFY = "MODIFY";
+export const DELETE = "DELETE";
 
-export const createPlanner = () => {
-  return { type: ACTION };
+export const createTodo = (todo: string) => {
+  return { type: CREATE, todo };
+};
+
+export const checkTodo = (id: number) => {
+  return { type: CHECK, id };
+};
+
+export const modifyTodo = (id: number, todo: string) => {
+  return { type: MODIFY, id, todo };
+};
+
+export const deleteTodo = (id: number) => {
+  return { type: DELETE, id };
 };
