9cfad0ef83f2e4fafaf83bb59dfc4d4f5ad78d06
jinho1011 <gino9940@gmail.com>
2022-03-27 14:35:54 +0900

    feat: organize init route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Todo from "./pages/Todo";
+import Movie from "./pages/Movie";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Todo" element={<Todo />} />
+      <Route path="/Movie" element={<Movie />} />
+    </Routes>
+  );
 }
 
 export default App;
