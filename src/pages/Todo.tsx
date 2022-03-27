221668597302f6adc728a990ec0cf0abed99fde0
jinho1011 <gino9940@gmail.com>
2022-03-27 16:26:44 +0900

    feat: add selector and dispatch

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  createTodo,
+  checkTodo,
+  modifyTodo,
+  deleteTodo,
+} from "../store/todo/actions";
+import { IRootReducer } from "../interfaces/store";
 import Header from "../components/Header";
 
 function Todo() {
+  const todos = useSelector((state: IRootReducer) => state.todos.data);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <Header />
-      Todo
+      {/* {todos.map((todo) => {
+        return <div key={todo.id}>{todo.todo}</div>;
+      })} */}
     </div>
   );
 }
