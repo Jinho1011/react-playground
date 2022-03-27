ed667984d022eb40aafe4d3193b6ba9a185a88c6
jinho1011 <gino9940@gmail.com>
2022-03-27 23:05:28 +0900

    feat: add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Global } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -8,9 +9,11 @@ import Todo from "./pages/Todo";
 import Movie from "./pages/Movie";
 import style from "./styles";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Global styles={style} />
       <Header />
       <Routes>
@@ -18,7 +21,7 @@ function App() {
         <Route path="/Todo" element={<Todo />} />
         <Route path="/Movie" element={<Movie />} />
       </Routes>
-    </>
+    </QueryClientProvider>
   );
 }
 
