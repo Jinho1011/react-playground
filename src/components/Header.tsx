f35690205ab891072b635471937e4f48fd0dce1c
jinho1011 <gino9940@gmail.com>
2022-03-27 16:22:44 +0900

    style: Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,89 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { css } from "@emotion/react";
 import { Link, useLocation } from "react-router-dom";
 
+const header = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 16px 0 13px 0;
+  -webkit-box-shadow: 4px 4px 20px 10px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 4px 4px 20px 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 4px 4px 20px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const nav__container = css`
+  width: 920px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const nav__logo = css`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const nav__items = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+`;
+
+const link__item = css`
+  text-decoration: none;
+  color: #525252;
+  padding-bottom: 3px;
+`;
+
+const link__selected__item = css`
+  text-decoration: none;
+  color: #1a1a1a;
+  font-weight: bold;
+  border-bottom: 3px solid #1a1a1a;
+`;
+
 function Header() {
-  let location = useLocation();
+  const location = useLocation();
+  const links = [
+    {
+      to: "/",
+      name: "Home",
+    },
+    {
+      to: "/todo",
+      name: "Todo",
+    },
+    {
+      to: "/movie",
+      name: "Movie",
+    },
+  ];
 
   return (
-    <div>
-      전진호
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/Todo">Todo</Link> |{" "}
-        <Link to="/Movie">Movie</Link>
+    <div css={header}>
+      <nav css={nav__container}>
+        <div css={nav__logo}>전진호</div>
+        <div css={nav__items}>
+          {links.map((link) => {
+            return (
+              <Link
+                css={
+                  location.pathname === link.to
+                    ? link__selected__item
+                    : link__item
+                }
+                to={link.to}
+                key={link.to}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </div>
       </nav>
     </div>
   );
