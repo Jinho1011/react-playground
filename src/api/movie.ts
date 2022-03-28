c9c2c6f4db1e5ad946306c63212df89268ade096
jinho1011 <gino9940@gmail.com>
2022-03-28 13:06:19 +0900

    feat: add movie detail api

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -1,8 +1,17 @@
 import { YTS } from "../interfaces/api/movie";
+import { Detail } from "../interfaces/api/detail";
 
 export const getMovieList = async (page: number): Promise<YTS> => {
   const response = await fetch(
     `https://yts.mx/api/v2/list_movies.json?limit=6&sort_by=like_count&page=${page}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+export const getMovieById = async (id: number): Promise<Detail> => {
+  const response = await fetch(
+    `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
   );
   const data = await response.json();
   return data;
